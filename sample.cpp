5f109a673ec0dfbf1f7b2ae35f60df3c2d98d20c
pkufahl <peter@dlvr.com>
2015-08-17 17:18:53 -0700

    first time commit

---
+++ sample.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Peter Kufahl on 8/17/15.
+//
+
+#include "sample.h"
+
+int main()
+{
+	std::cout << "hello world" << std::endl;
+	return 0;
+}
---
+++ sample.h
@@ -0,0 +1,9 @@
+//
+// Created by Peter Kufahl on 8/17/15.
+//
+
+#ifndef SAMPLE_SAMPLE_H
+#define SAMPLE_SAMPLE_H
+
+#include <iostream>
+#endif //SAMPLE_SAMPLE_H
